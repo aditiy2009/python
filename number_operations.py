7a9edeba11a38a6638c588f45874ee7d64e60523
Aditi <42689232+aditiy2009@users.noreply.github.com>
2018-12-30 11:36:44 +0530

    Add files via upload

---
+++ number_operations.py
@@ -0,0 +1,27 @@
+print("I will now count my chickens.")
+
+#operations occurs according to BODMAS
+print("Hens", 25+30/6)
+
+#% this is mod operations
+#gives remainder as output
+print("Roosters", 100-25*3%4)
+
+print("Now I will count eggs.")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# <,>,<=,>= all have output as true or false
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2? =", 3 + 2)
+print("What is 5 - 7? =", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
